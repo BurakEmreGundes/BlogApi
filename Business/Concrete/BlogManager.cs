30a3ba007eabd0436bef4125d742cac4721266b5
BurakEmreGundes <63010381+BurakEmreGundes@users.noreply.github.com>
2021-09-21 22:47:14 +0300

    Fluent validation dahil edildi

--- Business/Concrete/BlogManager.cs
+++ Business/Concrete/BlogManager.cs
@@ -1,8 +1,10 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +23,15 @@ namespace Business.Concrete
             
         public IResult Add(Blog blog)
         {
+            var context = new ValidationContext<Blog>(blog);
+            BlogValidator blogValidator = new BlogValidator();
+            var result=blogValidator.Validate(context);
+
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+
             _blogDal.Add(blog);
             return new SuccessResult(Messages.Added);
         }
